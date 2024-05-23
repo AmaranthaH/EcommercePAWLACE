92c4dec8747864c2ad868ea993178695378fc20d
Mariela González <166076072+marielabi@users.noreply.github.com>
2024-05-22 23:55:57 -0600

    Registro y Bienvenida

---
+++ src/nosotras/navbar.js
@@ -0,0 +1,8 @@
+
+
+const navToggle = document.querySelector(".nav-toggle")
+const navMenu = document.querySelector(".nav-menu")
+
+navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("nav-menu-visible");
+})
