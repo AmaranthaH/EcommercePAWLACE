5a8ffd7a62870ff0df1b9decfbe0404ecf058876
keniaestrada <kenialbsg@gmail.com>
2024-06-07 12:29:06 -0600

    registro de usuarios

--- src/bienvenidaRregistro/registro.js
+++ src/bienvenidaRregistro/registro.js
@@ -75,3 +75,34 @@ signupForm.addEventListener('submit', (e) => {
         passwordValidInput.reportValidity();
     }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('formRegistro').addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevenir el envío del formulario
+
+        // Obtener valores de los campos
+        const nombreCompleto = document.getElementById('UserName').value;
+        const nombreUsuario = document.getElementById('nickName').value;
+        const email = document.getElementById('userEmail').value;
+        const telefono = document.getElementById('telefono').value;
+        const password = document.getElementById('passwordUser').value;
+        const passwordValid = document.getElementById('passwordValid').value;
+
+        // Validar que las contraseñas coincidan
+        if (password !== passwordValid) {
+            alert('Las contraseñas no coinciden');
+            return;
+        }
+
+        // Guardar la información del usuario en Local Storage
+        localStorage.setItem('userNombreCompleto', nombreCompleto);
+        localStorage.setItem('userNombreUsuario', nombreUsuario);
+        localStorage.setItem('userEmail', email);
+        localStorage.setItem('userTelefono', telefono);
+        localStorage.setItem('userPassword', password);
+
+        alert('Registro exitoso');
+        // Redireccionar a la página de inicio de sesión
+        window.location.href = '../inicioSesion/inicio_de_sesion.html';
+    });
+});
