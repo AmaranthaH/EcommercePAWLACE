bef2d9bd90744a88b3e68e0007f25051128cc0fa
Viridiana Castillo <166076128+Viriiviriibamba@users.noreply.github.com>
2024-06-11 22:50:56 -0600

    Change reseñascatalogo.js

--- src/catalogo/catalogo.js
+++ src/catalogo/catalogo.js
@@ -57,6 +57,18 @@ document.addEventListener("DOMContentLoaded", function() {
         categoryDiv.appendChild(row);
         catalog.appendChild(categoryDiv);
     });
+    //Para las reseñas 
+    const stars = document.querySelectorAll('.star');
+    stars.forEach(function(star, index) {
+        star.addEventListener("click", function() {
+        for (let i = 0; i <= index; i++) {
+            stars[i].classList.add("checked");
+        }
+        for (let i = index + 1; i < stars.length; i++) {
+            stars[i].classList.remove("checked");
+        }
+    });
+});
 
     catalog.addEventListener('click', function(event) {
         if (event.target.classList.contains('personaliza-btn')) {
